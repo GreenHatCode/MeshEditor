c8727673df3b9cf2e1a23dca8c191f49af0df1d8
GreenHatCode <12gans3456@gmail.com>
2025-03-15 17:00:59 +0200

    Added header implementation for Cube, Sphere and Split

--- MeshEditor/Cube.h
+++ MeshEditor/Cube.h
@@ -1,5 +1,13 @@
 #pragma once
-class Cube
+#include "Command.h"
+
+class Cube : public Command
 {
+public:
+	const std::string& getName() const override;
+	int execute(const std::map<std::string, std::string>& args) override;
+
+private:
+	// todo: implement
 };
 
--- MeshEditor/Split.h
+++ MeshEditor/Split.h
@@ -1,5 +1,13 @@
 #pragma once
-class Split
+#include "Command.h"
+
+class Split : public Command
 {
+public:
+	const std::string& getName() const override;
+	int execute(const std::map<std::string, std::string>& args) override;
+
+private:
+	// todo: implement
 };
 
