ae1adc50619e847c82c81ba094efa13ce4c092de
GreenHatCode <12gans3456@gmail.com>
2025-03-15 16:33:39 +0200

    Added command interface

--- MeshEditor/Command.h
+++ MeshEditor/Command.h
@@ -1,6 +1,13 @@
 #pragma once
+#include <string>
+#include <map>
+
+// shows the command interface
 
 class Command 
 {
-
+public:
+	virtual ~Command(){}
+	virtual const std::string& getName() const = 0;
+	virtual int execute(const std::map<std::string, std::string>& args) = 0;
 };
