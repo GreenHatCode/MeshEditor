da586bc9a01be382981087587eb07b7359482f48
GreenHatCode <12gans3456@gmail.com>
2025-03-15 21:32:35 +0200

    Added reading command from console

--- MeshEditor/Application.cpp
+++ MeshEditor/Application.cpp
@@ -1,1 +1,22 @@
 #include "Application.h"
+
+void Application::registerCommand(std::unique_ptr<Command> command)
+{
+	//m_commandmap.emplace((*command).getName(),command);
+}
+
+int Application::execute(int argc, char* argv[])
+{
+	std::string input_command;
+
+	// main loop
+	std::cout << "\tMESH EDITOR" << std::endl;
+	while (true)
+	{
+		std::cout << "command> ";
+		std::getline(std::cin, input_command);
+		std::cout << input_command << std::endl;
+	}
+
+	return 0;
+}
