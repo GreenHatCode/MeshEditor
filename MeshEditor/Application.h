4857e785ffb2a5fd4917b9389046faa709d9dd7e
GreenHatCode <12gans3456@gmail.com>
2025-03-15 16:25:30 +0200

    Added comments to Application.h

--- MeshEditor/Application.h
+++ MeshEditor/Application.h
@@ -5,7 +5,10 @@
 class Application
 {
 public:
+	// stores all commands inside the application to enable a future extension with new commands
 	void registerCommand(std::unique_ptr<Command> command);
+	// call appropriate commands depending on the C arguments received from the main function
+	// also converts argc and argv to a dictionary understandable to each command
 	int execute(int argc, char* argv[]);
 
 private:
