a2f32a6e7b07d3f8227cbd9ab26bb4c477ea144a
GreenHatCode <12gans3456@gmail.com>
2025-03-15 16:22:13 +0200

    Added implementation for Application.h

--- MeshEditor/Application.h
+++ MeshEditor/Application.h
@@ -1,5 +1,14 @@
 #pragma once
+#include "Command.h"
+#include <iostream>
+
 class Application
 {
+public:
+	void registerCommand(std::unique_ptr<Command> command);
+	int execute(int argc, char* argv[]);
+
+private:
+	//todo
 };
 
