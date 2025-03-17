2ad7766390cf579f05303ea62b2b2795acd08e17
GreenHatCode <12gans3456@gmail.com>
2025-03-18 00:07:15 +0200

    Added input command parsing. Successfully tested on Cube, Sphere and Split commands

--- MeshEditor/Application.cpp
+++ MeshEditor/Application.cpp
@@ -19,7 +19,7 @@ int Application::execute(int argc, char* argv[])
 
 		if (auto search = m_commandmap.find(input_command.substr(0, input_command.find_first_of(' '))); search != m_commandmap.end())
 		{
-
+			// todo: validate()
 
 
 			search->second->execute(GetCommandArgs(input_command));
@@ -38,12 +38,33 @@ std::map<std::string, std::string> Application::GetCommandArgs(std::string comma
 	// remove command name
 	command.erase(0, command.find(' ')+1);
 	
-	// remove all spaces
-	const auto no_space_end = std::remove(command.begin(), command.end(), ' ');
+	// remove all spaces and quotes
+	auto no_space_end = std::remove(command.begin(), command.end(), ' ');
+	command.erase(no_space_end, command.end());
+	no_space_end = std::remove(command.begin(), command.end(), '\"');
 	command.erase(no_space_end, command.end());
 
+	// parsing
 	std::map<std::string, std::string> result_map;
 	std::string key, val;
+
+	// after preparing for parsing
+	// L=10.0,origin=(4.5,3.4,2.1),filepath="D:\cube.stl"
+
+	command.push_back('\0');
+	size_t prev = 0;
+	for (size_t curr = 1; curr < command.size(); curr++)
+	{
+		if (command[curr] == '\0' || (command[curr] == ',' && std::isalpha(std::tolower(command[curr + 1]))))
+		{
+			std::string param = command.substr(prev, curr - prev);
+			prev = curr + 1;
+
+			// fill the map with params
+			int delim_idx = param.find('=');
+			result_map.emplace(param.substr(0, delim_idx), param.substr(delim_idx + 1, param.size()));
+		}
+	}
 
 	return result_map;
 }
