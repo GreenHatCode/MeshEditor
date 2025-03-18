a015f4ce23ffb8ed21b2269df71848615e49845a
GreenHatCode <12gans3456@gmail.com>
2025-03-18 21:48:44 +0200

    Added necessary functional for Vec and Triangle. Also added printing triangles in file. Not tested.

--- MeshEditor/STLParser.cpp
+++ MeshEditor/STLParser.cpp
@@ -5,10 +5,10 @@ void STLParser::write(const TriangleSoup& triangleSoup, const std::string& filen
 	std::string content;
 	content += "solid MeshEditor\n";
 
-
-
-
-
+	for (size_t i = 0; i < triangleSoup.size(); i++)
+	{
+		content += print_triangle(triangleSoup[i]);
+	}
 
 	content += "endsolid MeshEditor";
 
@@ -17,4 +17,19 @@ void STLParser::write(const TriangleSoup& triangleSoup, const std::string& filen
 	otf.write(content.c_str(), content.size());
 	otf.flush();
 	otf.close();
+}
+
+std::string STLParser::print_triangle(Triangle triangle)
+{
+	std::ostringstream printed_triangle;
+
+	printed_triangle << "facet normal " << triangle.normal.x << " " << triangle.normal.y << " " << triangle.normal.z << std::endl;
+	printed_triangle << "outer loop" << std::endl;
+	printed_triangle << "vertex " << triangle.A.x << " " << triangle.A.y << " " << triangle.A.z << std::endl;
+	printed_triangle << "vertex " << triangle.B.x << " " << triangle.B.y << " " << triangle.B.z << std::endl;
+	printed_triangle << "vertex " << triangle.C.x << " " << triangle.C.y << " " << triangle.C.z << std::endl;
+	printed_triangle << "endloop" << std::endl;
+	printed_triangle << "endfacet" << std::endl;
+
+	return printed_triangle.str();
 }
