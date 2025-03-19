7ff72e45a97c9c4417dce4350fedcbd93760a2e4
GreenHatCode <12gans3456@gmail.com>
2025-03-19 21:49:00 +0200

    Added cube creation

--- MeshEditor/Cube.cpp
+++ MeshEditor/Cube.cpp
@@ -22,11 +22,42 @@ int Cube::execute(const std::map<std::string, std::string>& args)
 	double L = std::stod(args.at("L"));
 	Vec origin = get_origin(args.at("origin"));
 
-	// calculating of all 8 cube vertices
+	// calculating the coordinates of all 8 vertices of the cube
+	Vec A = origin + Vec(L / 2, -L / 2, 0);
+	Vec B = origin + Vec(-L / 2, -L / 2, 0);
+	Vec C = origin + Vec(-L / 2, L / 2, 0);
+	Vec D = origin + Vec(L / 2, L / 2, 0);
+	Vec A_top = A + Vec(0, 0, L);
+	Vec B_top = B + Vec(0, 0, L);
+	Vec C_top = C + Vec(0, 0, L);
+	Vec D_top = D + Vec(0, 0, L);
+
+	// creating the cude from triangles
+	TriangleSoup cube;
+	// bottom face
+	cube.push_back(Triangle(A, B, C));
+	cube.push_back(Triangle(A, D, C));
+	// front face
+	cube.push_back(Triangle(A, A_top, D_top));
+	cube.push_back(Triangle(A, D, D_top));
+	// right face
+	cube.push_back(Triangle(D, D_top, C_top));
+	cube.push_back(Triangle(D, C, C_top));
+	// back face
+	cube.push_back(Triangle(C, B, B_top));
+	cube.push_back(Triangle(C, C_top, B_top));
+	// left face
+	cube.push_back(Triangle(B, A, A_top));
+	cube.push_back(Triangle(B, B_top, A_top));
+	// top face
+	cube.push_back(Triangle(A_top, B_top, C_top));
+	cube.push_back(Triangle(A_top, D_top, C_top));
+
+
 
 
 	STLParser stlParser;
-	stlParser.write(TriangleSoup(), args.at("filepath"));
+	stlParser.write(cube, args.at("filepath"));
 
     return 0;
 }
