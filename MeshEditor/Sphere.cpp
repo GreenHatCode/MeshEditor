0cb960ee415309060cd38eb9e45c86394bbf6e16
GreenHatCode <12gans3456@gmail.com>
2025-03-24 18:44:23 +0200

    Added args checking and outputing finish codes

--- MeshEditor/Sphere.cpp
+++ MeshEditor/Sphere.cpp
@@ -7,6 +7,14 @@ const std::string& Sphere::getName() const
 
 int Sphere::execute(const std::map<std::string, std::string>& args)
 {
+	if (args.count("R") != 1 || args.count("origin") != 1 || args.count("slices") != 1 || args.count("rings") != 1 || args.count("filename") != 1) return 3;
+	if (std::stod(args.at("R")) <= 0)return 1;
+	if (std::stod(args.at("slices")) <= 2 || std::stod(args.at("rings")) <= 1)return 2;
+	std::ofstream test(args.at("filename"));
+	if (!test)return 2;
+
+
+
 	return 0;
 }
 
