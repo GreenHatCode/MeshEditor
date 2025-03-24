87c41dcede6377c5df455370439d86886893d96e
GreenHatCode <12gans3456@gmail.com>
2025-03-24 20:13:26 +0200

    Added converting args to nums

--- MeshEditor/Sphere.cpp
+++ MeshEditor/Sphere.cpp
@@ -13,12 +13,40 @@ int Sphere::execute(const std::map<std::string, std::string>& args)
 	std::ofstream test(args.at("filename"));
 	if (!test)return 2;
 
+	double R = std::stod(args.at("R"));
+	Vec origin = get_origin(args.at("origin")); // I consider it the center of the sphere
+	unsigned long slices = std::stoul(args.at("slices"));
+	unsigned long rings = std::stoul(args.at("rings"));
 
+	TriangleSoup sphere_mesh;
+
+	for (unsigned long m = 0; m < rings; m++)
+	{
+		for (unsigned long n = 0; n < slices; n++)
+		{
+
+		}
+	}
+
+
+
+
+
+	STLParser stlParser;
+	stlParser.write(sphere_mesh, args.at("filename"));
 
 	return 0;
 }
 
 Vec Sphere::get_origin(const std::string str_origin)
 {
-	return Vec();
+	double e[3];
+	size_t prev = 1;
+	for (size_t i = 0; i < 3; i++)
+	{
+		size_t curr = str_origin.find_first_of(",)", prev);
+		e[i] = std::stod(str_origin.substr(prev, curr - prev));
+		prev = curr + 1;
+	}
+	return Vec(e[0], e[1], e[2]);
 }
